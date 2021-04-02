a8565e8385abd4314d07b79d8d1449aff0ea7c91
Pudderz <mpudney6@gmail.com>
2021-04-02 18:50:16 +0100

    added mapbox map to map page

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+import ReactMapGl from 'react-map-gl';
+// app.js
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+
 
 export const Map = () => {
+    const [viewport, setViewport] = useState({
+        latitude: 51.509865,
+        longitude: -0.118092,
+        zoom: 8,
+        width: window.innerWidth,
+        height: window.innerHeight,
+    })
+
+    const handleViewportChange =(viewport)=>{
+        setViewport(viewport);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize",()=>{
+            setViewport({...viewport,
+                width: window.innerWidth,
+                height: window.innerHeight,
+            })
+        })
+        return () => {
+            window.removeEventListener("resize",()=>{
+                setViewport({...viewport,
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                })
+            })
+        }
+    }, [])
+
+
+    const goToNYC = () => {
+        setViewport({...viewport, longitude: -74.1, latitude: 40.7, zoom:8});
+    }
+
+    const goToLondon = () => {
+        setViewport({...viewport, latitude: 51.509865,
+            longitude: -0.118092, zoom:8});
+    }
+
     return (
-        <div>
-            Map
+        <div className=" w-screen h-screen overflow-hidden p-0">
+            <div>
+              <button onClick={goToNYC}>New York City</button>  
+              <button onClick={goToLondon}>London</button>  
+            </div>
+            <ReactMapGl 
+            className="w-full h-full overflow-hidden p-0"
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            {...viewport} onViewportChange={handleViewportChange} 
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            >
+                {/* <Marker
+                latitude={51.509865}
+                longitude={-0.118092}
+                >
+
+                </Marker> */}
+
+            </ReactMapGl>
+
+            
         </div>
     )
 }
