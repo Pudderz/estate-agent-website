7d1d3524121d8f712877b048a464f4a2c4455670
Pudderz <mpudney6@gmail.com>
2021-04-02 18:51:52 +0100

    edited tailwind and added styles folder for tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,83 @@
-
-import './App.css';
+import "./App.css";
 import PropertyPage from "./pages/PropertyPage.jsx";
 import Home from "./pages/Home.jsx";
 import Map from "./pages/Map.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
 
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { SignIn } from "./pages/SignIn";
+import { Footer } from "./components/Shared/Footer";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App min-h-screen">
       <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/map">Map Page</Link>
-            </li>
-            <li>
-              <Link to="/property-search">Search Page</Link>
-            </li>
-            <li>
-              <Link to="/property">Property Page</Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <nav className="w-full bg-gray-700 py-2 sticky top-0">
+            <ul className="flex flex-nowrap justify-between h-auto">
+              <li className="pd-4  w-full h-full">
+                <Link
+                  className="font-bold px-4 py-1 rounded focus:outline-none focus:shadow-outline text-white mx-4 hover:text-gray-200"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="pd-4 bg-gray-700 w-full h-full">
+                <Link
+                  className="font-bold px-4 py-1 rounded focus:outline-none focus:shadow-outline text-white mx-4 hover:text-gray-200"
+                  to="/map"
+                >
+                  Map Page
+                </Link>
+              </li>
+              <li className="pd-4 bg-gray-700 w-full h-full">
+                <Link
+                  className="font-bold  rounded focus:outline-none focus:shadow-outline text-white mx-4 hover:text-gray-200"
+                  to="/property-search"
+                >
+                  Search Page
+                </Link>
+              </li>
+              <li className="pd-4 bg-gray-700 w-full h-full">
+                <Link
+                  className="font-bold px-4 py-1 rounded focus:outline-none focus:shadow-outline text-white mx-4 hover:text-gray-200"
+                  to="/property"
+                >
+                  Property Page
+                </Link>
+              </li>
+              <li className="pd-4 bg-gray-700 w-full h-full">
+                <Link
+                  className="font-bold px-4 py-1 rounded focus:outline-none focus:shadow-outline text-white mx-4 hover:text-gray-200"
+                  to="/sign-in"
+                >
+                  Sign In
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/property-search">
-            <SearchPage />
-          </Route>
-          <Route path="/map">
-            <Map />
-          </Route>
-          <Route path="/property">
-            <PropertyPage/>
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router> 
+          <Switch>
+            <Route path="/property-search">
+              <SearchPage />
+            </Route>
+            <Route path="/map">
+              <Map />
+            </Route>
+            <Route path="/sign-in">
+              <SignIn/>
+            </Route>
+            <Route path="/property">
+              <PropertyPage />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <Footer/> 
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //purge all unused styles in producation
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}','./src/pages/*.{jsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
