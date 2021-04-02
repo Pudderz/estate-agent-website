77a1dd694fe6a6464a0045774155e37ad1a07141
Pudderz <mpudney6@gmail.com>
2021-04-02 18:53:23 +0100

    added Home.jsx to pages folder

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Footer } from "../components/Shared/Footer.jsx";
 
 export const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>HomePage</h2>
+      {/* Home
+      <h2 className="text-4x1"> Hello World</h2>
+      <p className="text-blue-300 text-4x1 bg-gray-900">dsa</p>
 
+      <div className="bg-green-500">
+          <h1 className="text-red-500"> Yoooooo</h1>
+      </div> */}
+      
+    </div>
+  );
+};
 
 export default Home;
