209358ac39f5b28563ae9ee5d13765f290fec3f2
Pudderz <mpudney6@gmail.com>
2021-04-15 22:21:38 +0100

    designed simple footer

--- frontend/src/components/Shared/Footer.jsx
+++ frontend/src/components/Shared/Footer.jsx
@@ -2,21 +2,54 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="bg-gray-800 w-full h-64 text-white sticky top-full">
-      <ul className="flex justify-around mx-auto max-w-7xl p-2">
-        <li>
-          <h4>Column 1</h4>
-        </li>
-        <li>
-          <h4>Column 2</h4>
-        </li>
-        <li>
-          <h4>Column 3</h4>
-        </li>
-        <li>
-          <h4>Column 4</h4>
-        </li>
-      </ul>
-    </div>
+
+      <footer
+        className="sticky top-full text-white min-h-full py-10 bg-gray-800 w-full h-64"
+      
+      >
+        <div
+          className="footer"
+        
+        >
+          <div
+          className="flex flex-col gap-5 text-current"
+          >
+            <h3>Pages</h3>
+            <a href="">Home Page</a>
+            <a href="">Map Page</a>
+            <a href="">Search Page</a>
+            <a href="">Sign In</a>
+          
+          </div>
+          <div
+           
+          >
+            <h3>External Pages</h3>
+            <a href="https://github.com/Pudderz">Github</a>
+            <a href="www.matthewpudney.co.uk" >Portfolio</a>
+          </div>
+
+          <div
+          
+          >
+            <h3>Contact Me</h3>
+            <p style={{margin:'0'}}>mpudney2@gmail.com</p>
+          </div>
+        </div>
+        {/* <div
+        className="subFooter"
+      
+        >
+          <p>E-Commerce Site</p>
+          <div
+            
+          >
+            <p>Terms & Conditions</p>
+            <p>Security policy</p>
+            <p>Return policy</p>
+          </div>
+        </div> */}
+      </footer>
+
   );
 };
